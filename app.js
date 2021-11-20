440a19723b6f1f6c424411da999c6d93fb027791
justyna0p <justyna.pedziach@gmail.com>
2021-11-20 18:56:07 +0100

    feat: create server using express

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const http = require('http');
+const express = require('express');
 const port = 3000;
 
-const handler = (request, response) => {
-    console.log("new user!")
-    response.end("hello user")
-};
-const server = http.createServer(handler);
 
-server.listen(port, (err) => {
-    if (err) {
-        return console.log('something went bad')
-    }
+const app = express();
 
-    console.log("server is running...")
+app.get('/', (request, response) => {
+    response.send("Hello user!!!!")
 });
+
+app.get('/contact', (request, response) => {
+    response.send("Contact details")
+});
+
+app.listen(port);
+
+

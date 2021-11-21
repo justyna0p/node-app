e2f1d6460bc8efba3c44e303a7df1398e6cca612
justyna0p <justyna.pedziach@gmail.com>
2021-11-21 18:39:44 +0100

    style: add semicolons

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const port = 3000;
 
 
 const app = express();
-app.set('view engine', 'hbs')
+app.set('view engine', 'hbs');
 
 app.get('/', (request, response) => {
-    response.render("index")
+    response.render("index");
 });
 
 app.get('/contact', (request, response) => {
-    response.send("Contact details")
+    response.send("Contact details");
 });
 
 app.listen(port);
